a0bb4d3abb23a914c822aadd7545c2eebffbe1fa
gopal <vgopalooty@gmail.com>
2018-11-12 11:38:28 +0530

    cc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -167,105 +167,106 @@ return redirect('/');
     
 public function testreturns(){
 
-$activeHoldings= holding::where('status',1)->orderBy('timestamp', 'asc')->with('schemes')->with('return_credit')->get();
-foreach ($activeHoldings as $key => $holding){
+// $activeHoldings= holding::where('status',1)->orderBy('timestamp', 'asc')->with('schemes')->with('return_credit')->get();
+// foreach ($activeHoldings as $key => $holding){
     
-    $currentTime = Carbon::now();
-    $referralReturnDuration = $holding->schemes->referralReturnDuration;
-    $duration =   $holding->schemes->duration;
-    $totalCycles = $holding->schemes->referralReturnFrequency;
+//     $currentTime = Carbon::now();
+//     $referralReturnDuration = $holding->schemes->referralReturnDuration;
+//     $duration =   $holding->schemes->duration;
+//     $totalCycles = $holding->schemes->referralReturnFrequency;
   
    
   
 
 
-   for($cycle=1;$cycle<=$totalCycles;$cycle++){
-    $holdingDate = Carbon::createFromFormat('Y-m-d H:i:s',$holding->TIMESTAMP);
-    $holdingDate->addDays($referralReturnDuration*$cycle);
+//    for($cycle=1;$cycle<=$totalCycles;$cycle++){
+//     $holdingDate = Carbon::createFromFormat('Y-m-d H:i:s',$holding->TIMESTAMP);
+//     $holdingDate->addDays($referralReturnDuration*$cycle);
    
-    if($currentTime->greaterThan($holdingDate)){
-        echo "true";
+//     if($currentTime->greaterThan($holdingDate)){
+//         echo "true";
 
-        $isCycleCredited = DB::table('return_credits')->where('holdingId', $holding->id)->where('returnCycle', $cycle)->first();
-      if(!$isCycleCredited) {
-         //credit the returns 
-         $returnAmount =  ($holding->schemes->referralReturnRate/100 )* $holding->amount;
-         $returnUser = DB::table('users')->where('id', $holding->userId)->first()->referralid;
-         $from =DB::table('profiles')->where('userId', $holding->userId)->first();
-         $fromName = $from->firstName . ' ' . $from->lastName;
-         echo $fromName;
+//         $isCycleCredited = DB::table('return_credits')->where('holdingId', $holding->id)->where('returnCycle', $cycle)->first();
+//       if(!$isCycleCredited) {
+//          //credit the returns 
+//          $returnAmount =  ($holding->schemes->referralReturnRate/100 )* $holding->amount;
+//          $returnUser = DB::table('users')->where('id', $holding->userId)->first()->referralid;
+//          $from =DB::table('profiles')->where('userId', $holding->userId)->first();
+//          $fromName = $from->firstName . ' ' . $from->lastName;
+//          echo $fromName;
 
-         //transaction
-         $transaction= new transaction;
-         $transaction->userId = $returnUser;
-         $transaction->TYPE= 'Cr.';
-         $transaction->amount = $returnAmount;
-         $transaction->shadowAccount = 'prc';
-         $transaction->ACCOUNT = 'pw';
-         $transaction->narration= $cycle."rd ". "Referral comission from : " .$fromName. " ". $holding->userId  ;
-         //
-        $sms = 'Catcotrade Credit USD:'.$returnAmount. " Month : ". $cycle." ". "Referral comission from : " .$fromName. " ". $holding->userId  ;
-         //save return credit true 
-         $return_credit = new return_credit;
-         $return_credit->userId= $holding->userId;
-         $return_credit->holdingId= $holding->id;
-         $return_credit->returnCycle=$cycle;
+//          //transaction
+//          $transaction= new transaction;
+//          $transaction->userId = $returnUser;
+//          $transaction->TYPE= 'Cr.';
+//          $transaction->amount = $returnAmount;
+//          $transaction->shadowAccount = 'prc';
+//          $transaction->ACCOUNT = 'pw';
+//          $transaction->narration= $cycle."rd ". "Referral comission from : " .$fromName. " ". $holding->userId  ;
+//          //
+//         $sms = 'Catcotrade Credit USD:'.$returnAmount. " Month : ". $cycle." ". "Referral comission from : " .$fromName. " ". $holding->userId  ;
+//          //save return credit true 
+//          $return_credit = new return_credit;
+//          $return_credit->userId= $holding->userId;
+//          $return_credit->holdingId= $holding->id;
+//          $return_credit->returnCycle=$cycle;
 
            
-         //
+//          //
         
-         DB::transaction(function () use ($transaction,$return_credit) {
-            $transaction->save();
-            $return_credit->transactionId= $transaction->id;
-$return_credit->save();
+//          DB::transaction(function () use ($transaction,$return_credit) {
+//             $transaction->save();
+//             $return_credit->transactionId= $transaction->id;
+// $return_credit->save();
            
 
-        });
+//         });
 
-        //send sms
-        $smsUser = DB::table('users')->where('id', $holding->userId)->first()->referralid;
-        $smsUserProfile =DB::table('profiles')->where('userId', $smsUser)->first();
-        $mobile= $smsUserProfile->isdcode.$smsUserProfile->mobile;
-        operations::sendSMS($mobile,$sms);
+//         //send sms
+//         $smsUser = DB::table('users')->where('id', $holding->userId)->first()->referralid;
+//         $smsUserProfile =DB::table('profiles')->where('userId', $smsUser)->first();
+//         $mobile= $smsUserProfile->isdcode.$smsUserProfile->mobile;
+//         operations::sendSMS($mobile,$sms);
         
 
-         exit();
+//          exit();
 
-      } 
+//       } 
         
-   }
+//    }
    
 
-   }
+//    }
 
-//maturity logic 
-$holdingDate = Carbon::createFromFormat('Y-m-d H:i:s',$holding->TIMESTAMP);
-$holdingDate->addDays($holding->schemes->duration);
-if($currentTime->greaterThan($holdingDate)){
+// //maturity logic 
+// $holdingDate = Carbon::createFromFormat('Y-m-d H:i:s',$holding->TIMESTAMP);
+// $holdingDate->addDays($holding->schemes->duration);
+// if($currentTime->greaterThan($holdingDate)){
 
-    echo "maturity";
-    $maturityAmount = ($holding->amount*($holding->schemes->maturityRate)/100)+$holding->amount;
-    $transaction= new transaction;
-    $transaction->userId = $holding->userId;
-    $transaction->TYPE= 'Cr.';
-    $transaction->amount = $maturityAmount;
-    $transaction->ACCOUNT = 'mw';
-    $transaction->narration= 'Credit investment return' ;
-    $sms = 'Catcotrade Credit investment return : USD '. $maturityAmount ;  
-    DB::transaction(function () use ($transaction) {
-        $transaction->save();
+//     echo "maturity";
+//     $maturityAmount = ($holding->amount*($holding->schemes->maturityRate)/100)+$holding->amount;
+//     $transaction= new transaction;
+//     $transaction->userId = $holding->userId;
+//     $transaction->TYPE= 'Cr.';
+//     $transaction->amount = $maturityAmount;
+//     $transaction->ACCOUNT = 'mw';
+//     $transaction->narration= 'Credit investment return' ;
+//     $sms = 'Catcotrade Credit investment return : USD '. $maturityAmount ;  
+//     DB::transaction(function () use ($transaction) {
+//         $transaction->save();
         
        
 
-    });
-    holding::where('id',$holding->id)->update(['STATUS' => 0]);
-    $smsUserProfile =DB::table('profiles')->where('userId', $holding->userId)->first();
-    $mobile= $smsUserProfile->isdcode.$smsUserProfile->mobile;
-    operations::sendSMS($mobile,$sms);
+//     });
+//     holding::where('id',$holding->id)->update(['STATUS' => 0]);
+//     $smsUserProfile =DB::table('profiles')->where('userId', $holding->userId)->first();
+//     $mobile= $smsUserProfile->isdcode.$smsUserProfile->mobile;
+//     operations::sendSMS($mobile,$sms);
 
-}
-}
+// }
+// }
 
+//test return ends
 }
 //return $activeHoldings;
 
