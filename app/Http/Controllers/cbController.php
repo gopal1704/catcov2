6bddf7193217a1bb7316476f2c95aa355a9b0fd6
gopal <vgopalooty@gmail.com>
2018-12-02 23:15:31 +0530

    cc

--- app/Http/Controllers/cbController.php
+++ app/Http/Controllers/cbController.php
@@ -9,6 +9,7 @@ class cbController extends Controller
     //
 
     public function  cb(Request $request){
-        
+        return response('Hello World', 200)
+                  ->header('Content-Type', 'text/plain');
     }
 }
