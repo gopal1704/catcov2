79fd0001094f2ed5808820c3f6cea9e7a0f1c36f
gopal <vgopalooty@gmail.com>
2018-12-03 09:56:36 +0530

    cc

--- app/Http/Controllers/cbController.php
+++ app/Http/Controllers/cbController.php
@@ -10,11 +10,14 @@ class cbController extends Controller
     //
 
     public function  cb(Request $request){
+        Log::debug('An informational message.');
 
         $data=json_decode($request->getContent(), true);
         $code=$data->event->data->code;
         App\coinbasepayment::where('code', $code)
           ->update(['status' => 1]);
+          Log::debug('An informational message.');
+
         return response('', 200);
     }
 }
